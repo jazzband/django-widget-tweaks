447189fb36beb9f3d02eff9c64cfc92ac6a1eb03
Mike Korobov <kmike84@gmail.com>
2011-02-03 14:45:22 +0500

    More tests

--- widget_tweaks/tests.py
+++ widget_tweaks/tests.py
@@ -11,8 +11,12 @@ class MyForm(Form):
                  }))
     with_cls = CharField(widget=TextInput(attrs={'class':'class0'}))
 
-def render_field(field, filter, params):
-    render_field_str = '{{ form.%s|%s:"%s" }}' % (field, filter, params)
+def render_field(field, filter, params, *args):
+    filters = [(filter, params)]
+    filters.extend(zip(args[::2], args[1::2]))
+    filter_strings = ['|%s:"%s"' % (f[0], f[1],) for f in filters]
+    render_field_str = '{{ form.%s%s }}' % (field, ''.join(filter_strings))
+
     tpl = Template("{% load widget_tweaks %}" + render_field_str)
     context = Context({'form': MyForm()})
     return tpl.render(context)
@@ -31,6 +35,14 @@ class SimpleAttrTest(TestCase):
         assertIn('id="id_simple"', res)
         assertIn('foo="bar"', res)
 
+    def test_attr_chaining(self):
+        res = render_field('simple', 'attr', 'foo:bar', 'attr', 'bar:baz')
+        assertIn('type="text"', res)
+        assertIn('name="simple"', res)
+        assertIn('id="id_simple"', res)
+        assertIn('foo="bar"', res)
+        assertIn('bar="baz"', res)
+
     def test_add_class(self):
         res = render_field('simple', 'add_class', 'foo')
         assertIn('class="foo"', res)
@@ -39,6 +51,10 @@ class SimpleAttrTest(TestCase):
         res = render_field('simple', 'add_class', 'foo bar')
         assertIn('class="foo bar"', res)
 
+    def test_add_class_chaining(self):
+        res = render_field('simple', 'add_class', 'foo', 'add_class', 'bar')
+        assertIn('class="bar foo"', res)
+
 class CustomizedWidgetTest(TestCase):
     def test_attr(self):
         res = render_field('with_attrs', 'attr', 'foo:bar')
@@ -46,12 +62,30 @@ class CustomizedWidgetTest(TestCase):
         assertNotIn('foo="baz"', res)
         assertIn('egg="spam"', res)
 
+    def test_attr_chaining(self):
+        res = render_field('with_attrs', 'attr', 'foo:bar', 'attr', 'bar:baz')
+        assertIn('foo="bar"', res)
+        assertNotIn('foo="baz"', res)
+        assertIn('egg="spam"', res)
+        assertIn('bar="baz"', res)
+
     def test_attr_class(self):
         res = render_field('with_cls', 'attr', 'foo:bar')
         assertIn('foo="bar"', res)
         assertIn('class="class0"', res)
 
+    def test_default_attr(self):
+        res = render_field('with_cls', 'attr', 'type:search')
+        assertIn('class="class0"', res)
+        assertIn('type="search"', res)
+
     def test_add_class(self):
         res = render_field('with_cls', 'add_class', 'class1')
         assertIn('class0', res)
         assertIn('class1', res)
+
+    def test_add_class_chaining(self):
+        res = render_field('with_cls', 'add_class', 'class1', 'add_class', 'class2')
+        assertIn('class0', res)
+        assertIn('class1', res)
+        assertIn('class2', res)
