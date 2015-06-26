c7c2d7f2812e357469ac2b5e5198cfdefd0e47c1
Mikhail Korobov <kmike84@gmail.com>
2015-06-27 04:10:12 +0500

    switch to setuptools
    wheels are nice

--- setup.py
+++ setup.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
-from distutils.core import setup
-for cmd in 'egg_info', 'develop':
-    import sys
-    if cmd in sys.argv:
-        from setuptools import setup
+from setuptools import setup
 
-version='1.3'
+version = '1.3'
 
 setup(
     name='django-widget-tweaks',
@@ -26,12 +22,12 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.5',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
 )
