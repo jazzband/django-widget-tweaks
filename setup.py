e0592d30a697821543142fea2f916458fc863cea
Mikhail Korobov <kmike84@gmail.com>
2016-12-09 22:08:23 +0500

    drop Python 2.6 and Python 3.2 support

--- setup.py
+++ setup.py
@@ -22,12 +22,11 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
 )
