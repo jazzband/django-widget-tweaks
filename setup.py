7b87049ab62e4a9f6acae266432cc7975eeff193
Aleksi Häkli <aleksi.hakli@iki.fi>
2020-03-08 18:14:07 +0200

    Deprecate Python 2.7 support

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     description='Tweak the form field rendering in templates, not in python-level form definitions.',
     long_description=open('README.rst').read() + "\n\n" + open('CHANGES.rst').read(),
     license='MIT license',
-    requires=['django (>= 1.8)'],
+    requires=['django (>=1.11)'],
     packages=['widget_tweaks', 'widget_tweaks.templatetags'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -20,13 +20,10 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
 )
