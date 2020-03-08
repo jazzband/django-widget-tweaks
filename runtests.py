74ccd4578af977a8c36712cfe6f4446a55853968
Aleksi Häkli <aleksi.hakli@iki.fi>
2020-03-08 18:44:21 +0200

    Use automatic formatting for all files

--- runtests.py
+++ runtests.py
@@ -8,13 +8,11 @@ from django.core.management import call_command
 
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
-opts = {'INSTALLED_APPS': ['widget_tweaks']}
+opts = {"INSTALLED_APPS": ["widget_tweaks"]}
 
 if django.VERSION[:2] >= (1, 10):
-    opts['TEMPLATES'] = [
-        {
-            'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        },
+    opts["TEMPLATES"] = [
+        {"BACKEND": "django.template.backends.django.DjangoTemplates",},
     ]
 
 settings.configure(**opts)
@@ -25,6 +23,8 @@ if __name__ == "__main__":
         # Allow running specific tests with arguments e.g.
         # tox -e py36-django-20 -- tests.RenderFieldTagFieldReuseTest.test_field_datetime_widget
         test_name = sys.argv[1]
-        call_command('test', 'widget_tweaks.{test_name}'.format(test_name=test_name), verbosity=2)
+        call_command(
+            "test", "widget_tweaks.{test_name}".format(test_name=test_name), verbosity=2
+        )
     else:
-        call_command('test', 'widget_tweaks', verbosity=2)
+        call_command("test", "widget_tweaks", verbosity=2)
--- setup.py
+++ setup.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-version = '1.4.5'
+version = "1.4.5"
 
 setup(
-    name='django-widget-tweaks',
+    name="django-widget-tweaks",
     version=version,
-    author='Mikhail Korobov',
-    author_email='kmike84@gmail.com',
-    url='https://github.com/jazzband/django-widget-tweaks',
-    description='Tweak the form field rendering in templates, not in python-level form definitions.',
-    long_description=open('README.rst').read() + "\n\n" + open('CHANGES.rst').read(),
-    license='MIT license',
-    requires=['django (>=1.11)'],
-    packages=['widget_tweaks', 'widget_tweaks.templatetags'],
+    author="Mikhail Korobov",
+    author_email="kmike84@gmail.com",
+    url="https://github.com/jazzband/django-widget-tweaks",
+    description="Tweak the form field rendering in templates, not in python-level form definitions.",
+    long_description=open("README.rst").read() + "\n\n" + open("CHANGES.rst").read(),
+    license="MIT license",
+    requires=["django (>=1.11)"],
+    packages=["widget_tweaks", "widget_tweaks.templatetags"],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Topic :: Software Development :: Libraries :: Python Modules'
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
