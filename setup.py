cef3c5477abdba86bb9c74e6356109c6ae613a5d
Adam Johnson <me@adamj.eu>
2022-08-25 17:34:00 +0100

    Add Django 4.1 support

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
         "Framework :: Django :: 2.2",
         "Framework :: Django :: 3.2",
         "Framework :: Django :: 4.0",
+        "Framework :: Django :: 4.1",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
