f7b67d7cee2d325c3ab90db0c06a4062b7019ea2
Adam Johnson <me@adamj.eu>
2023-04-24 16:17:38 +0100

    and Django 4.2

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         "Framework :: Django :: 3.2",
         "Framework :: Django :: 4.0",
         "Framework :: Django :: 4.1",
+        "Framework :: Django :: 4.2",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
