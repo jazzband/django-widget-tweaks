8afacd8c09cefe39a8c590251564aa16f34a1750
David Nugent <davidn@uniquode.io>
2023-07-14 11:26:16 +1000

    Update version determination

--- widget_tweaks/__init__.py
+++ widget_tweaks/__init__.py
@@ -1,7 +1,3 @@
-from pkg_resources import get_distribution, DistributionNotFound
+from importlib.metadata import version
 
-try:
-    __version__ = get_distribution("django-widget-tweaks").version
-except DistributionNotFound:
-    # package is not installed
-    __version__ = None
+__version__ = version("django-widget-tweaks")
