19e64a2b8e9a168dabda190a7c0aa8d0a5335114
Adam Johnson <me@adamj.eu>
2023-10-25 11:42:21 +0100

    Add Django 5.0 support

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
         "Framework :: Django :: 3.2",
         "Framework :: Django :: 4.1",
         "Framework :: Django :: 4.2",
+        "Framework :: Django :: 5.0",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
